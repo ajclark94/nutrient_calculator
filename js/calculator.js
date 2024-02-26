54025645a4bf77622f9fc5ccbc2b35ae1747dc43
ajclark94 <115447290+ajclark94@users.noreply.github.com>
2024-02-26 15:09:15 -0700

    add ingredients to printout

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,5 @@
 const calculator= document.getElementById('calculator');
+const inputs = document.getElementById('inputs')
 const submitBtn = document.getElementById('submit-btn');
 const calculateBtn = document.getElementById('calculate-btn');
 const results = document.getElementById('results');
@@ -7,9 +8,14 @@ const inputPortion = document.getElementById('new-portion');
 const inputUOM = document.getElementById('units')
 const nutrientNames = ["Calories", "Protein", "Carbohydrate", "Total Fat", "Saturated Fat", "Polyunsaturated Fat", "Trans Fat", "Cholesterol", "Sodium", "Potassium", "Phosphorus", "Calcium", "Iron", "Total Fiber", "Sugar", "Fluid", "Vitamin A", "Vitamin C", "Vitamin B1", "Vitamin B2", "Niacin", "Vitamin B6", "Magnesium", "Zinc", "Copper", "Vitamin E", "Pantothenic Acid", "Vitamin B12", "Folate", "Vitamin K", "Selenium", "Vitamin D", "Iodine", " Chloride", "Biotin", "Fluoride", "Choline", "Linoleic Acid", "Manganese", "Molybdenum", "Chromium", "Carb Cnt"];
 
-const itemObj = JSON.parse(localStorage.getItem('item'));
+// const itemObj = JSON.parse(localStorage.getItem('item'));
+const itemObj = "./assets/items.JSON"
+fetch(itemObj)
+    .then(x => JSON.parse(x.text))
+    .then
 let itemNames;
-const resultArray = []
+const resultArray = [];
+let newIngredientArray = [];
 
 const addInputs = () => {
     calculator.innerHTML = ""
@@ -30,7 +36,7 @@ const addInputs = () => {
         selected.setAttribute('selected', true);
         select.prepend(selected)
         select.classList += "ingredient"
-        calculator.appendChild(select);
+        inputs.appendChild(select);
     }
     itemNames = document.getElementsByClassName('ingredient');
 }
@@ -42,9 +48,17 @@ const calculate = () => {
         return 1;
     } else {
         const arrays = selectedItems.map((itemName) => itemObj[itemName]['nutrientArr']);
+        const ingredientArray = selectedItems.map((itemName) => itemObj[itemName]['ingredientsArr'])
         for (let i = 0; i < arrays[0].length; i++) {
             resultArray[i] = arrays.reduce((sum, arr) => sum + arr[i], 0)
         }
+        for (let i = 0; i < ingredientArray.length; i++) {
+            newIngredientArray = newIngredientArray.concat(ingredientArray[i])
+        }
+        const ingredientsSet = new Set(newIngredientArray);
+        const uniqueArr = [...ingredientsSet]
+        console.log(ingredientsSet);
+        const ingredientsStr = uniqueArr.join(', ')
         console.log(resultArray);
         results.innerHTML += `
         <h4>${inputName.value}</h4>
@@ -55,6 +69,7 @@ const calculate = () => {
             let currValue = resultArray[i];
             results.innerHTML += `<p>${currNutrient}: ${currValue}`
         }
+        results.innerHTML += `Ingredients: ${ingredientsStr}`
     }
     
 }
