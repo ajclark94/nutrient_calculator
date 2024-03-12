c6e07942070774b2a691e42bc57bb5b6dbaa3ff0
austin <115447290+ajclark94@users.noreply.github.com>
2024-03-12 09:20:26 -0600

    add css for pages, move app.js to main dir

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "postgres",
+  user: "pi",
+  host: "192.168.1.26",
+  database: "ingredients",
+  password: "Newsletter!",
   port: 5432,
 });
 
 client.connect();
 
 app.get("/", (_req, res) => {
-  res.sendFile(__dirname + "../item.html");
+  res.sendFile(__dirname + "/item.html");
 });
 
 app.post("/submit", async (req, res) => {
@@ -73,7 +73,7 @@ app.post("/submit", async (req, res) => {
   } = req.body;
 
   const query = {
-    text: "INSERT INTO items (item_name, portion_size, uom, recipe_number, calories, protein, carbohydrate, total_fat, saturated_fat, polyunsaturated_fat, trans_fat, cholesterol, sodium, potassium, phosphorus, calcium, iron, total_fiber, sugar, fluid, vitamin_a, vitamin_c, vitamin_b1, vitamin_b2, niacin, vitamin_b6, magnesium, zinc, copper, vitamin_e, pantothenic_acid, vitamin_b12, folate, vitamin_k selenium, vitamin_d, iodine, chloride, biotin, fluoride, choline, linoleic_acid, manganese, molybdenum, chromium, carb_cnt, ingredients) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50)",
+    text: "INSERT INTO items (item_name, portion_size, uom, recipe_number, calories, protein, carbohydrate, total_fat, saturated_fat, polyunsaturated_fat, trans_fat, cholesterol, sodium, potassium, phosphorus, calcium, iron, total_fiber, sugar, fluid, vitamin_a, vitamin_c, vitamin_b1, vitamin_b2, niacin, vitamin_b6, magnesium, zinc, copper, vitamin_e, pantothenic_acid, vitamin_b12, folate, vitamin_k selenium, vitamin_d, iodine, chloride, biotin, fluoride, choline, linoleic_acid, manganese, molybdenum, chromium, carb_cnt, ingredients) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47)",
     values: [
       item_name,
       portion_size,
--- js/calculator.js
+++ js/calculator.js
@@ -1,80 +1,126 @@
 //test comment
 
-const calculator= document.getElementById('calculator');
-const inputs = document.getElementById('inputs')
-const submitBtn = document.getElementById('submit-btn');
-const calculateBtn = document.getElementById('calculate-btn');
-const results = document.getElementById('results');
-const inputName = document.getElementById('new-name');
-const inputPortion = document.getElementById('new-portion');
-const inputUOM = document.getElementById('units')
-const nutrientNames = ["Calories", "Protein", "Carbohydrate", "Total Fat", "Saturated Fat", "Polyunsaturated Fat", "Trans Fat", "Cholesterol", "Sodium", "Potassium", "Phosphorus", "Calcium", "Iron", "Total Fiber", "Sugar", "Fluid", "Vitamin A", "Vitamin C", "Vitamin B1", "Vitamin B2", "Niacin", "Vitamin B6", "Magnesium", "Zinc", "Copper", "Vitamin E", "Pantothenic Acid", "Vitamin B12", "Folate", "Vitamin K", "Selenium", "Vitamin D", "Iodine", " Chloride", "Biotin", "Fluoride", "Choline", "Linoleic Acid", "Manganese", "Molybdenum", "Chromium", "Carb Cnt"];
+const calculator = document.getElementById("calculator");
+const inputs = document.getElementById("inputs");
+const submitBtn = document.getElementById("submit-btn");
+const calculateBtn = document.getElementById("calculate-btn");
+const results = document.getElementById("results");
+const inputName = document.getElementById("new-name");
+const inputPortion = document.getElementById("new-portion");
+const inputUOM = document.getElementById("units");
+const nutrientNames = [
+  "Calories",
+  "Protein",
+  "Carbohydrate",
+  "Total Fat",
+  "Saturated Fat",
+  "Polyunsaturated Fat",
+  "Trans Fat",
+  "Cholesterol",
+  "Sodium",
+  "Potassium",
+  "Phosphorus",
+  "Calcium",
+  "Iron",
+  "Total Fiber",
+  "Sugar",
+  "Fluid",
+  "Vitamin A",
+  "Vitamin C",
+  "Vitamin B1",
+  "Vitamin B2",
+  "Niacin",
+  "Vitamin B6",
+  "Magnesium",
+  "Zinc",
+  "Copper",
+  "Vitamin E",
+  "Pantothenic Acid",
+  "Vitamin B12",
+  "Folate",
+  "Vitamin K",
+  "Selenium",
+  "Vitamin D",
+  "Iodine",
+  "Chloride",
+  "Biotin",
+  "Fluoride",
+  "Choline",
+  "Linoleic Acid",
+  "Manganese",
+  "Molybdenum",
+  "Chromium",
+  "Carb Cnt",
+];
 
-// const itemObj = JSON.parse(localStorage.getItem('item'));
-const itemObj = "./assets/items.JSON"
-fetch(itemObj)
-    .then(x => JSON.parse(x.text))
-    .then
+// replace with db info
+const itemObj = JSON.parse(localStorage.getItem("item"));
+
 let itemNames;
 const resultArray = [];
 let newIngredientArray = [];
 
 const addInputs = () => {
-    calculator.innerHTML = ""
-    let count = document.getElementById('count').value
-    for (let i = 0; i < count; i++) {
-        const select = document.createElement('select')
-        for (const key in itemObj) {
-            if (itemObj.hasOwnProperty(key)) {
-                const option = document.createElement('option');
-                option.value = key;
-                option.text = key;
-                select.prepend(option)
-            }
-        }
-        const selected = document.createElement('option');
-        selected.value = "--Select--";
-        selected.text = "--Select--";
-        selected.setAttribute('selected', true);
-        select.prepend(selected)
-        select.classList += "ingredient"
-        inputs.appendChild(select);
+  calculator.innerHTML = "";
+  let count = document.getElementById("count").value;
+  for (let i = 0; i < count; i++) {
+    const select = document.createElement("select");
+    for (const key in itemObj) {
+      if (itemObj.hasOwnProperty(key)) {
+        const option = document.createElement("option");
+        option.value = key;
+        option.text = key;
+        select.prepend(option);
+      }
     }
-    itemNames = document.getElementsByClassName('ingredient');
-}
+    const selected = document.createElement("option");
+    selected.value = "--Select--";
+    selected.text = "--Select--";
+    selected.setAttribute("selected", true);
+    select.prepend(selected);
+    select.classList += "ingredient";
+    inputs.appendChild(select);
+  }
+  itemNames = document.getElementsByClassName("ingredient");
+};
 
 const calculate = () => {
-    const selectedItems = Array.from(itemNames).map((select) => select.value);
-    if (selectedItems.indexOf('--Select--') !== -1) {
-        alert("Please select an ingredient in each input or reduce the Number of ingredients")
-        return 1;
-    } else {
-        const arrays = selectedItems.map((itemName) => itemObj[itemName]['nutrientArr']);
-        const ingredientArray = selectedItems.map((itemName) => itemObj[itemName]['ingredientsArr'])
-        for (let i = 0; i < arrays[0].length; i++) {
-            resultArray[i] = arrays.reduce((sum, arr) => sum + arr[i], 0)
-        }
-        for (let i = 0; i < ingredientArray.length; i++) {
-            newIngredientArray = newIngredientArray.concat(ingredientArray[i])
-        }
-        const ingredientsSet = new Set(newIngredientArray);
-        const uniqueArr = [...ingredientsSet]
-        console.log(ingredientsSet);
-        const ingredientsStr = uniqueArr.join(', ')
-        console.log(resultArray);
-        results.innerHTML += `
+  const selectedItems = Array.from(itemNames).map((select) => select.value);
+  if (selectedItems.indexOf("--Select--") !== -1) {
+    alert(
+      "Please select an ingredient in each input or reduce the Number of ingredients",
+    );
+    return 1;
+  } else {
+    const arrays = selectedItems.map(
+      (itemName) => itemObj[itemName]["nutrientArr"],
+    );
+    const ingredientArray = selectedItems.map(
+      (itemName) => itemObj[itemName]["ingredientsArr"],
+    );
+    for (let i = 0; i < arrays[0].length; i++) {
+      resultArray[i] = arrays.reduce((sum, arr) => sum + arr[i], 0);
+    }
+    for (let i = 0; i < ingredientArray.length; i++) {
+      newIngredientArray = newIngredientArray.concat(ingredientArray[i]);
+    }
+    const ingredientsSet = new Set(newIngredientArray);
+    const uniqueArr = [...ingredientsSet];
+    console.log(ingredientsSet);
+    const ingredientsStr = uniqueArr.join(", ");
+    console.log(resultArray);
+    results.innerHTML += `
         <h4>${inputName.value}</h4>
         <p>Portion Size: ${inputPortion.value} ${inputUOM.value}</p>
-        `
-        for (let i = 0; i < nutrientNames.length; i++) {
-            let currNutrient = nutrientNames[i];
-            let currValue = resultArray[i];
-            results.innerHTML += `<p>${currNutrient}: ${currValue}`
-        }
-        results.innerHTML += `Ingredients: ${ingredientsStr}`
+        `;
+    for (let i = 0; i < nutrientNames.length; i++) {
+      let currNutrient = nutrientNames[i];
+      let currValue = resultArray[i];
+      results.innerHTML += `<p>${currNutrient}: ${currValue}`;
     }
-    
-}
+    results.innerHTML += `Ingredients: ${ingredientsStr}`;
+  }
+};
 
-submitBtn.addEventListener('click', addInputs)
-calculateBtn.addEventListener('click', calculate)
+submitBtn.addEventListener("click", addInputs);
+calculateBtn.addEventListener("click", calculate);
